42441e4f89ac2a76114f292590f2ab73ab2ee240
Dima Kozyrev <bvckslvsh@gmail.com>
2023-02-03 14:22:18 +0300

    Some database stuff.

--- src/main/java/me/bvckslvsh/emarket/controller/ContentController.java
+++ src/main/java/me/bvckslvsh/emarket/controller/ContentController.java
@@ -1,5 +1,8 @@
 package me.bvckslvsh.emarket.controller;
 
+import me.bvckslvsh.emarket.domain.Content;
+import me.bvckslvsh.emarket.repository.ContentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,8 +10,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ContentController {
 
+    @Autowired
+    private ContentRepository contentRepository;
+
+
     @GetMapping("/content/new")
     public String newContent(Model model){
+        Iterable<Content> contents = contentRepository.findAll();
+        model.addAttribute("contents", contents);
         return "content";
     }
 
