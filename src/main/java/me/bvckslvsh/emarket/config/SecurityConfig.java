7ec0ff113cc1b602ec14630762c4fe6194ab7846
bvckslvsh <bvckslvsh@gmail.com>
2023-02-09 13:46:08 +0300

    Added test users.

--- src/main/java/me/bvckslvsh/emarket/config/SecurityConfig.java
+++ src/main/java/me/bvckslvsh/emarket/config/SecurityConfig.java
@@ -4,11 +4,37 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("qwerty")
+                .roles("admin")
+                .build();
+
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("qwerty")
+                .roles("user")
+                .build();
+
+        UserDetails owner = User.withDefaultPasswordEncoder()
+                .username("owner")
+                .password("qwerty")
+                .roles("owner")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, user, owner);
+    }
 
     @Bean
     public SecurityFilterChain configure (HttpSecurity httpSecurity) throws Exception {
@@ -22,4 +48,5 @@ public class SecurityConfig {
 
                 return httpSecurity.build();
     }
+
 }
